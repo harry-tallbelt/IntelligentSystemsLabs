65d8062e75a92762bb94f7bc529d909ea2091523
Igor <harry.tallbelt@gmail.com>
2016-10-26 12:54:30 +0300

    Immutable Range (+Parameter fix)

--- src/CoreLogic/Range.cs
+++ src/CoreLogic/Range.cs
@@ -4,7 +4,8 @@ namespace CoreLogic
 {
 	public struct Range
 	{
-        public double LowerBoundary, UpperBoundary;
+        public double LowerBoundary { get; private set; }
+        public double UpperBoundary { get; private set; }
 
         public bool IsEmpty { get { return LowerBoundary > UpperBoundary; } }
 
